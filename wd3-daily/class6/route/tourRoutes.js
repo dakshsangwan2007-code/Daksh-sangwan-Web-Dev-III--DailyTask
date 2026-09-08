eacf2d8a3de6da7b33cfe38d655721ce3b3ce476
dakshsangwan2007-code <daksh.sangwan2007@gmail.com>
2026-09-08 23:22:21 +0530

    Delete wd3-daily/class6 directory

--- wd3-daily/class6/route/tourRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-const router = express.Router();
-const tourController = require("..controller.getAllTours");
-
-//get all tours
-router.get("/tours", tourController.getALLTours)
-module.exports = router;
